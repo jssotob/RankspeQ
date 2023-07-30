e74a403220c52a179c3a3f69ac7b336c6615f121
Jonatan_Soto <J.S.Soto@cgiar.org>
2023-07-30 10:30:59 -0500

    yield arguments replaced by target.trait.file/name. Help module updated

--- R/function_yield_calibration.R
+++ R/function_yield_calibration.R
@@ -1,8 +1,8 @@
 #' Comparisons between raknings and yield or trait of interest.
 #'
 #' @param ranks The list obtained by MSPQ_ranks.
-#' @param yield.file data.frame in narrow format with the yield component or any other trait to contrast against MultispeQ ranking. An observation per experimental unit is required as well as the row & column coordinates if spats = TRUE in MSPQ_ranks.
-#' @param yield.name Character. name of the yield variable on yield.file to contrast against MultispeQ ranking.
+#' @param target.trait.file data.frame in narrow format with the yield component or any other trait to contrast against MultispeQ ranking. An observation per experimental unit is required as well as the row & column coordinates if spats = TRUE in MSPQ_ranks.
+#' @param target.trait.name Character. name of the yield variable on target.trait.file to contrast against MultispeQ ranking.
 #' @param metadata data.frame in narrow format with extra information about the genotypes i.e. gene pool, known biotic/abiotic traits, etc. Optional process. NULL by default.
 #'
 #' @return A list with the confusion matrices and summary of predictions.
@@ -13,23 +13,23 @@
 #' # yield_data: any table containing the same evaluated genotypes and the yield or any trait of interest to contrast against rankings
 #'
 #' yield_calibration(ranks = ranks,
-#'                   yield.file = yield_data,
-#'                   yield.name = kg_ha,
+#'                   target.trait.file = yield_data,
+#'                   target.trait.name = kg_ha,
 #'                   metadata = NULL)
 #' }
-yield_calibration <- function(ranks, yield.file, yield.name, metadata = NULL){
+yield_calibration <- function(ranks, target.trait.file, target.trait.name, metadata = NULL){
 
 
 # yield calibration -------------------------------------------------------
 
 
-  if(!is.null(yield.file)){
-    if(!is.data.frame(yield.file)){
-      stop(paste0("The argument yield.file must be a data frame that contains the '", ranks$Genotype, "' column and the yield component to evaluate."))
-    } else if(!ranks$Genotype %in% names(yield.file)){
-      stop(paste0("The variable '", ranks$Genotype ,"' was not found into the yield.file dataset."))
-    } else if(!all(unique(yield.file[,ranks$Genotype]) %in% unique(unlist(c(ranks$means[,ranks$Genotype]))))){
-      gen_index <- unique(yield.file[,ranks$Genotype])[which(!unique(yield.file[,ranks$Genotype]) %in% unique(unlist(c(ranks$means[,ranks$Genotype]))))]
+  if(!is.null(target.trait.file)){
+    if(!is.data.frame(target.trait.file)){
+      stop(paste0("The argument target.trait.file must be a data frame that contains the '", ranks$Genotype, "' column and the target trait to evaluate."))
+    } else if(!ranks$Genotype %in% names(target.trait.file)){
+      stop(paste0("The variable '", ranks$Genotype ,"' was not found into the target.trait.file dataset."))
+    } else if(!all(unique(target.trait.file[,ranks$Genotype]) %in% unique(unlist(c(ranks$means[,ranks$Genotype]))))){
+      gen_index <- unique(target.trait.file[,ranks$Genotype])[which(!unique(target.trait.file[,ranks$Genotype]) %in% unique(unlist(c(ranks$means[,ranks$Genotype]))))]
       gen_lets <- substr(gen_index, 1, 3)
 
       gen_real <- list()
@@ -39,10 +39,10 @@ yield_calibration <- function(ranks, yield.file, yield.name, metadata = NULL){
       gen_real <- unlist(gen_real)
 
       if(!length(gen_real) == 0){
-      stop(paste0("The genotype(s) '", paste0((gen_index), collapse = "', '"), "' from the yield file not found into the ranked genotypes.
+      stop(paste0("The genotype(s) '", paste0((gen_index), collapse = "', '"), "' from the target trait file not found into the ranked genotypes.
                   Should it be named as '",paste0((gen_real), collapse = "', '") , "'?"))
       } else{
-        stop(paste0("The genotype(s) '", paste0((gen_index), collapse = "', '"), "' from the yield file not found into the ranked genotypes."))
+        stop(paste0("The genotype(s) '", paste0((gen_index), collapse = "', '"), "' from the target trait file not found into the ranked genotypes."))
       }
     }
   }
@@ -55,17 +55,17 @@ yield_calibration <- function(ranks, yield.file, yield.name, metadata = NULL){
 
 
   if(!is.null(ranks[["SPATS_variables"]])){
-    if(!all(ranks[["SPATS_variables"]] %in% names(yield.file))){
+    if(!all(ranks[["SPATS_variables"]] %in% names(target.trait.file))){
       stop(paste0("The MulstispeQ ranks obtained by the function MSPQ_ranks() were adjusted by SpATS. The spatial variables ", paste0("'",ranks[["SPATS_variables"]],"'", collapse = ", "),
-                " were not found into the yield file."))
-    } else if(!all(c(ranks$Sources_of_variation[DAS_ind:length(ranks$Sources_of_variation)], yield.name) %in% names(yield.file))){
-      pos <- which(!c(ranks$Sources_of_variation[DAS_ind:length(ranks$Sources_of_variation)], yield.name) %in% names(yield.file))
-      stop(paste0("The variable(s) ", paste0("'",c(ranks$Sources_of_variation[DAS_ind:length(ranks$Sources_of_variation)], yield.name)[pos],"'", collapse = ", "),
-                  " do not exist into the yield file. Make sure that it/they exist and try again"))
+                " were not found into the target trait file"))
+    } else if(!all(c(ranks$Sources_of_variation[DAS_ind:length(ranks$Sources_of_variation)], target.trait.name) %in% names(target.trait.file))){
+      pos <- which(!c(ranks$Sources_of_variation[DAS_ind:length(ranks$Sources_of_variation)], target.trait.name) %in% names(target.trait.file))
+      stop(paste0("The variable(s) ", paste0("'",c(ranks$Sources_of_variation[DAS_ind:length(ranks$Sources_of_variation)], target.trait.name)[pos],"'", collapse = ", "),
+                  " do not exist into the target trait file. Make sure that it/they exist and try again"))
       rm(pos, DAS_ind)
     }
 
-    cat("Adjusting yield with spatial components... \n")
+    cat("Adjusting target trait with spatial components... \n")
 
 
     if("DAS" %in% ranks$Sources_of_variation){
@@ -77,18 +77,18 @@ yield_calibration <- function(ranks, yield.file, yield.name, metadata = NULL){
     arr_SoV <- ranks$Sources_of_variation[sov_rm]
     rm(sov_rm)
 
-    yield.list <- as.list(group_split(yield.file, .dots = arr_SoV))
+    yield.list <- as.list(group_split(target.trait.file, .dots = arr_SoV))
     yield.list <- lapply(yield.list, function(x)as.data.frame(x))
 
     yield.model <- list()
     yield.blup <- list()
 
     for(i in 1:length(yield.list)){
-      yield.model[[i]] <- suppressWarnings(SpATS_mrbean(data = yield.list[[i]], response = yield.name,genotype = ranks$Genotype, col = ranks$SPATS_variables[2], row =ranks$SPATS_variables[1], segm=T ,ncols=NULL, nrows=NULL, rep="",
+      yield.model[[i]] <- suppressWarnings(SpATS_mrbean(data = yield.list[[i]], response = target.trait.name,genotype = ranks$Genotype, col = ranks$SPATS_variables[2], row =ranks$SPATS_variables[1], segm=T ,ncols=NULL, nrows=NULL, rep="",
                                                         fix_fact = NULL, ran_fact = NULL, gen_ran = T, covariate = NULL,
                                                         clean_out= FALSE, iterations=1, checks = NULL))
       yield.blup[[i]] <-data.frame(predict(yield.model[[i]], which = ranks$Genotype, predFixed = "marginal")[,c(ranks$Genotype,"predicted.values")])
-      names(yield.blup[[i]])[2] <- yield.name
+      names(yield.blup[[i]])[2] <- target.trait.name
       yield.blup[[i]]$x = yield.list[[i]][,arr_SoV][1]
       for(s in 1:length(arr_SoV)){
         names(yield.blup[[i]])[2+s] <- arr_SoV[s]
@@ -109,7 +109,7 @@ yield_calibration <- function(ranks, yield.file, yield.name, metadata = NULL){
     arr_SoV <- ranks$Sources_of_variation[sov_rm]
     rm(sov_rm)
 
-    yield.transf <- yield.file %>%
+    yield.transf <- target.trait.file %>%
       group_by_(.dots = c(ranks$Genotype, arr_SoV)) %>%
       summarise_if(.predicate = "is.numeric", .funs = "mean", na.rm=T) %>%
       ungroup %>% as.data.frame
@@ -146,7 +146,7 @@ cat("Plotting confusion matrices... \n")
 
   for(i in 1:length(conam)){
 
-    conam[[i]]$y.rank <- as.numeric(rank(conam[[i]][,yield.name]))
+    conam[[i]]$y.rank <- as.numeric(rank(conam[[i]][,target.trait.name]))
     conam[[i]]$m.rank <- as.numeric(rank(conam[[i]][,"final_score"]))
     conam[[i]] %<>% arrange(y.rank)
     clus <- round(nrow(conam[[i]])*0.1)
